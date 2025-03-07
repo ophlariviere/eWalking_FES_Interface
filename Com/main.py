785208eff97de740c42aebe8c28c3bcb56d08980
ophlariviere <ophelie.lariviere@laposte.net>
2025-03-07 15:56:31 +0400

    up2

---
+++ Com/main.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from Stim_P24.Stim_Interface import StimInterfaceWidget
+from Received_data import DataReceiver
+import logging
+
+# Utilisez QThread pour exécuter DataReceiver en parallèle
+from PyQt5.QtCore import QThread
+
+
+# Configure le logging
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def main():
+    # Définition des paramètres du serveur
+    server_ip = "192.168.0.1" #   # "192.168.0.1" 127.0.0.1# Adresse IP du serveur
+    server_port = 7  # 7# 50000 Port à utiliser
+
+    # Créez une application PyQt5
+    app = QApplication(sys.argv)
+
+    # Créez une instance du widget de visualisation
+    visualization_widget = StimInterfaceWidget()
+    visualization_widget.show()
+
+    # Créez une instance de DataReceiver
+    data_receiver = DataReceiver(server_ip, server_port, visualization_widget)
+
+    class DataThread(QThread):
+
+        def __init__(self, receiver):
+            super().__init__()
+            self.receiver = receiver
+
+        def run(self):
+            self.receiver.start_receiving()
+
+    data_thread = DataThread(data_receiver)
+    data_thread.start()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
